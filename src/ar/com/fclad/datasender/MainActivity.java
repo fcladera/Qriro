a03e56690a18d16f4565b837da1da43639c845c4
Fernando Cladera <fclad@fclad.com.ar>
2013-11-14 18:40:58 -0300

    Basic service TCPclientService implemented. It can only connect/disconnect to server.

--- src/ar/com/fclad/datasender/MainActivity.java
+++ src/ar/com/fclad/datasender/MainActivity.java
@@ -16,8 +16,10 @@ import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -57,16 +59,16 @@ public class MainActivity extends Activity implements SensorEventListener{
 	private static final String localServer = "10.0.0.1";
 	private static final String remoteServer = "192.168.2.122";
 	private int port = 7777;
-	private static final int timeout = 1000;
-	private Socket socket;
-	PrintWriter writer = null;
-	private boolean isSending = false;
-	private boolean isConnected = false;
+	
 	
 	// UI elements
-	private ToggleButton sendData;
+	private ToggleButton testSensors;
 	private Button connectRemote;
 	private Button connectLocal;
+	private Button disconnect;
+	private Button startDraw;
+	
+	private BroadcastReceiver receiver;
 
 	
 	@Override
@@ -109,13 +111,50 @@ public class MainActivity extends Activity implements SensorEventListener{
 
 		// SOCKET
 		//new Thread(new ClientThread()).start();
-		Intent i = new Intent(this, TCPclientService.class);
+		
 		
 		// UI
 		
-		sendData = (ToggleButton) findViewById(R.id.sendData);
+		//sendData = (ToggleButton) findViewById(R.id.sendData);
 		connectLocal = (Button) findViewById(R.id.connect_Local);
 		connectRemote = (Button) findViewById(R.id.connect_Remote);
+		disconnect = (Button) findViewById(R.id.disconnect);
+		startDraw = (Button) findViewById(R.id.drawButton);
+		testSensors = (ToggleButton) findViewById(R.id.testSensors);
+		
+		
+		// RECEIVER
+		receiver = new BroadcastReceiver() {
+			
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				Bundle bundle = intent.getExtras();
+				if(bundle!=null){
+					switch(bundle.getInt(TCPclientService.STATUS)){
+					case TCPclientService.CONNECTED:
+						Log.d("MainActivity", "Service said connected");
+						connectLocal.setEnabled(false);
+						connectRemote.setEnabled(false);
+						startDraw.setEnabled(true);
+						disconnect.setEnabled(true);
+						testSensors.setEnabled(false);
+						
+						
+						return;
+					
+					case TCPclientService.DISCONNECT:
+						Log.d("MainActivity", "Service said disconnected");
+						connectLocal.setEnabled(true);
+						connectRemote.setEnabled(true);
+						startDraw.setEnabled(false);
+						disconnect.setEnabled(false);
+						testSensors.setEnabled(true);
+						return;
+					}
+				}
+				
+			}
+		};
 		
 	}
 	
@@ -138,6 +177,8 @@ public class MainActivity extends Activity implements SensorEventListener{
 	    		sensorRate);
 	    // connect to a socket that must be running in the server
 	    
+	    registerReceiver(receiver, new IntentFilter(TCPclientService.NOTIFICATION));
+	    
 	   
 	}
 
@@ -145,7 +186,11 @@ public class MainActivity extends Activity implements SensorEventListener{
 	protected void onPause() {
 		// unregister listener
 		super.onPause();
+		
+		unregisterReceiver(receiver);
+		
 		sensorManager.unregisterListener(this);
+		/*
 		if(socket!=null){
 			if(isSending){
 				destroyWriterStream();
@@ -155,7 +200,7 @@ public class MainActivity extends Activity implements SensorEventListener{
 			}
 			disconnect();
 			
-		}
+		}*/
 	    
 	}
 	
@@ -167,7 +212,8 @@ public class MainActivity extends Activity implements SensorEventListener{
 			Log.w("MainActivity","Toggled testSensor button");
 			testingSensors = ((ToggleButton) view).isChecked();
 		return;
-		case R.id.sendData:
+		//case R.id.sendData:
+			/*
 			isSending = ((ToggleButton) view).isChecked();
 			if(isSending){
 				createWriterStream();
@@ -177,7 +223,8 @@ public class MainActivity extends Activity implements SensorEventListener{
 			}
 				
 			Log.d("MainActivity", "Is sending "+isSending);
-			return;
+			*/
+		//	return;
 		case R.id.RotationVectActivated:
 			isRotationVectorActivated = ((CheckBox) view).isChecked();
 			Log.d("MainActivity","Toggled Rotation vector sensor");
@@ -190,8 +237,15 @@ public class MainActivity extends Activity implements SensorEventListener{
 		
 		switch(view.getId()){
 		case R.id.connect_Remote:
-			if(!isConnected){
-				Log.d("mainActivity", "Connection to remote asked");
+			Log.d("mainActivity", "Connection to remote asked");
+			Intent i = new Intent(this, TCPclientService.class);
+			i.putExtra(TCPclientService.COMMAND, TCPclientService.CONNECT);
+			i.putExtra(TCPclientService.PORT, port);
+			i.putExtra(TCPclientService.SERVER, remoteServer);
+			startService(i);
+			
+			/*if(!isConnected){
+				
 				new Connect().execute(remoteServer);
 			}
 			else{
@@ -200,9 +254,17 @@ public class MainActivity extends Activity implements SensorEventListener{
 					sendData.setChecked(false);
 					isSending=false;
 				}
-			}
+			}*/
 			return;
 		case R.id.connect_Local:
+			Log.d("mainActivity", "Connection to local");
+			Intent serviceIntent = new Intent(this, TCPclientService.class);
+			serviceIntent.putExtra(TCPclientService.COMMAND, TCPclientService.CONNECT);
+			serviceIntent.putExtra(TCPclientService.SERVER, localServer);
+			serviceIntent.putExtra(TCPclientService.PORT, port);
+			startService(serviceIntent);
+			
+			/*
 			if(!isConnected){
 				Log.d("mainActivity", "Connection to local asked");
 				new Connect().execute(localServer);
@@ -213,12 +275,18 @@ public class MainActivity extends Activity implements SensorEventListener{
 					sendData.setChecked(false);
 					isSending=false;
 				}
-			}
+			}*/
+			return;
+			
+		case R.id.disconnect:
+			Intent disconnect = new Intent(this, TCPclientService.class);
+			disconnect.putExtra(TCPclientService.COMMAND, TCPclientService.DISCONNECT);
+			startService(disconnect);
 			return;
 		case R.id.drawButton:
 			Intent intent = new Intent(MainActivity.this, DrawActivity.class);
 			startActivity(intent);
-			break;
+			return;
 		
 		}
 		
@@ -226,78 +294,38 @@ public class MainActivity extends Activity implements SensorEventListener{
 		
 	}
 	
-	private void disconnect(){
-		// disconnect socket
-		try {
-			socket.close();
-		} catch (IOException e) {
-			Log.e("MainActivity", "Error on disconnect");
-			//e.printStackTrace();
-		}
-		sendData.setEnabled(false);
-		connectRemote.setEnabled(true);
-		connectLocal.setEnabled(true);
-		isConnected = false;
-		socket = null;
-	}
+//	private void disconnect(){
+//		// disconnect socket
+//		try {
+//			//socket.close();
+//		} catch (IOException e) {
+//			Log.e("MainActivity", "Error on disconnect");
+//			//e.printStackTrace();
+//		}
+//		sendData.setEnabled(false);
+//		connectRemote.setEnabled(true);
+//		connectLocal.setEnabled(true);
+//		isConnected = false;
+//		socket = null;
+//	}
 
 	
-	private class Connect extends AsyncTask<String, Void, String>{
-		/**
-		 * Tries to connect in background to the socket.
-		 * If succed, UI is changed allowing to send data
-		 * If fails, show toast
-		 */
+	
 
-		@Override
-		protected String doInBackground(String... params) {
-				//Toast.makeText(getApplicationContext(), "Connecting to server "+params[0].toString(), Toast.LENGTH_SHORT).show();
-				InetSocketAddress serverAddr = new InetSocketAddress(params[0], port);
-				socket = new Socket();
-				try {
-					socket.connect(serverAddr, timeout);
-					isConnected = true;
-				} catch (IOException e) {
-					//e.printStackTrace();
-					Log.e("Socket","Connection error");
-					isConnected = false;
-				}
-				return params[0];
-		}
-		
-		protected void onPostExecute(String result){
-			if(isConnected){
-				Toast.makeText(getApplicationContext(), "Successfully connected", Toast.LENGTH_SHORT).show();
-				if(result==localServer){
-					connectRemote.setEnabled(false);
-				}
-				else{
-					
-					connectLocal.setEnabled(false);
-				}
-				sendData.setEnabled(true);
-			}
-			else{
-				Toast.makeText(getApplicationContext(), "Error connecting to "+result, Toast.LENGTH_SHORT).show();
-			}
-		}
-		
-	}
-	
-	private void createWriterStream(){
-		try {
-			 writer = new PrintWriter(new BufferedWriter(
-					new OutputStreamWriter(socket.getOutputStream())),
-					true);
-		} catch (IOException e) {
-			e.printStackTrace();
-			Log.e("MainActivity", "Error on createWriterStream");
-		}
-	}
-	
-	private void destroyWriterStream(){
-		writer = null;
-	}
+//	private void createWriterStream(){
+//		try {
+//			 writer = new PrintWriter(new BufferedWriter(
+//					new OutputStreamWriter(socket.getOutputStream())),
+//					true);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			Log.e("MainActivity", "Error on createWriterStream");
+//		}
+//	}
+//	
+//	private void destroyWriterStream(){
+//		writer = null;
+//	}
 	
 	
 
@@ -328,14 +356,14 @@ public class MainActivity extends Activity implements SensorEventListener{
 			if(testingSensors){
 				showAccelValues(event.values,dT);
 			}
-			if(isSending){
-	
-					String str = "A:"+code+":"+dT+":"+event.values[0]+":"+event.values[1]+":"+event.values[2]+";";
-					code++;
-					writer.println(str);
-					writer.flush();
-				}
-				
+//			if(isSending){
+//	
+//					String str = "A:"+code+":"+dT+":"+event.values[0]+":"+event.values[1]+":"+event.values[2]+";";
+//					code++;
+//					writer.println(str);
+//					writer.flush();
+//				}
+//				
 			}
 			
 		if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
@@ -348,12 +376,12 @@ public class MainActivity extends Activity implements SensorEventListener{
 			if(testingSensors){
 				showGyroValues(event.values,dT);
 			}
-			if(isSending){
-				String str = "G:"+code+":"+dT+":"+event.values[0]+":"+event.values[1]+":"+event.values[2]+";";
-				code++;
-				writer.println(str);
-				writer.flush();
-			}
+//			if(isSending){
+//				String str = "G:"+code+":"+dT+":"+event.values[0]+":"+event.values[1]+":"+event.values[2]+";";
+//				code++;
+//				writer.println(str);
+//				writer.flush();
+//			}
 			
 		}
 	
@@ -365,12 +393,12 @@ public class MainActivity extends Activity implements SensorEventListener{
 			if(testingSensors){
 				showRotationVectorValues(event.values, dT);
 			}
-			if(isSending){
-				String str = "R:"+code+":"+dT+":"+event.values[0]+":"+event.values[1]+":"+event.values[2]+";";
-				code++;
-				writer.println(str);
-				writer.flush();
-			}
+//			if(isSending){
+//				String str = "R:"+code+":"+dT+":"+event.values[0]+":"+event.values[1]+":"+event.values[2]+";";
+//				code++;
+//				writer.println(str);
+//				writer.flush();
+//			}
 		}
 		
 	}
--- src/ar/com/fclad/datasender/TCPclientService.java
+++ src/ar/com/fclad/datasender/TCPclientService.java
@@ -1,16 +1,214 @@
 package ar.com.fclad.datasender;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
 
 public class TCPclientService extends Service {
+	private Looper mServiceLooper;
+	private ServiceHandler mServiceHandler;
+	 
+	
+	
+	public static final String NOTIFICATION = "ar.com.fclad.datasender";
+	
+	public static final String COMMAND = "command";
+	public static final String SERVER = "server";
+	public static final String PORT = "port";
+	public static final int CONNECT = 1;
+	public static final int DISCONNECT = 0;
+	
+	public static final String STATUS = "status"; 
+	public static final int CONNECTED = 1;
+	public static final int DISCONNECTED = 0; 
+	public static final int GETSTATUS = 3;
+
+	
+	private Socket socket;
+	PrintWriter writer = null;
+	private boolean isSending = false;
+	private boolean isConnected = false;
+	
+	private String server = null;
+	private int port = -1;
+	
+	private static final int timeout = 1000;
+
+	
+	
 	public TCPclientService() {
+		
 	}
 
+	private final class ServiceHandler extends Handler {
+		public ServiceHandler(Looper looper) {
+			super(looper);
+	    }
+	    @Override
+	    public void handleMessage(Message msg) {
+	    	switch(msg.getData().getInt(COMMAND)){
+	    	case CONNECT:
+	    		if(!isConnected){
+	    			server = msg.getData().getString(SERVER);
+	    			port = msg.getData().getInt(PORT);
+	    			Log.d("TCPclienService","Connecting with server "+server+" port "+port);
+	    			new Connect().execute(server);
+	    			
+	    		}
+	    		break;
+	    	case DISCONNECT:
+	    		if(isConnected){
+	    			Log.d("TCPclienService","Disconnecting from server");
+	    			disconnect();
+	    			
+	    		}
+	    		stopSelf();	// Destroy service on disconnection
+	    			
+	    		break;
+	    	default:
+	    		break;
+	    	
+	    	}
+	    		    	
+	    	
+//	    	long endTime = System.currentTimeMillis() + 2*1000;
+//	    	while (System.currentTimeMillis() < endTime) {
+//	    		synchronized (this) {
+//	    			try {
+//	    				wait(endTime - System.currentTimeMillis());
+//	    				Log.w("ServiceHandler",""+msg.arg1);
+//	    				Log.w("ServiceHandler",""+msg.getData().getString("Test"));
+//	    			} catch (Exception e) {
+//	    			}
+//		      }
+//	    	}
+	    	
+	    	
+			
+			
+			
+			//stopSelf(msg.arg1);
+	    }
+	}
+
+	private class Connect extends AsyncTask<String, Void, String>{
+		/**
+		 * Tries to connect in background to the socket.
+		 * If succed, UI is changed allowing to send data
+		 * If fails, show toast
+		 */
+
+		@Override
+		protected String doInBackground(String... params) {
+				//Toast.makeText(getApplicationContext(), "Connecting to server "+params[0].toString(), Toast.LENGTH_SHORT).show();
+				InetSocketAddress serverAddr = new InetSocketAddress(params[0], port);
+				socket = new Socket();
+				try {
+					
+					socket.connect(serverAddr, timeout);
+					isConnected = true;
+					Intent intent = new Intent(NOTIFICATION); 
+					intent.putExtra(STATUS, CONNECTED);
+	    			sendBroadcast(intent);
+				} catch (IOException e) {
+					//e.printStackTrace();
+					Log.e("Socket","Connection error");
+					isConnected = false;
+					Intent intent = new Intent(NOTIFICATION);
+					intent.putExtra(STATUS, DISCONNECTED);
+					sendBroadcast(intent);
+					
+					stopSelf();
+				}
+				return params[0];
+		}
+		
+		protected void onPostExecute(String result){
+			if(isConnected){
+				Toast.makeText(getApplicationContext(), "Successfully connected to "+result, Toast.LENGTH_SHORT).show();
+				//if(result==localServer){
+					//connectRemote.setEnabled(false);
+			//	}
+				//else{
+					
+					//connectLocal.setEnabled(false);
+			//	}
+				//sendData.setEnabled(true);
+			}
+			else{
+				Toast.makeText(getApplicationContext(), "Error connecting to "+result, Toast.LENGTH_SHORT).show();
+				stopSelf();
+			}
+		}
+		
+	}
+	
+	private void disconnect(){
+		// disconnect socket
+		try {
+			socket.close();
+		} catch (IOException e) {
+			Log.e("MainActivity", "Error on disconnect");
+			stopSelf();
+			//e.printStackTrace();
+		}
+		//sendData.setEnabled(false);
+		//connectRemote.setEnabled(true);
+		//connectLocal.setEnabled(true);
+		isConnected = false;
+		socket = null;
+		Intent intent = new Intent(NOTIFICATION);
+		intent.putExtra(STATUS, DISCONNECTED);
+		sendBroadcast(intent);
+	}
+
+	
+	public void onCreate() {
+		HandlerThread thread = new HandlerThread("TCPclientThread");
+		thread.start();
+		Log.w("TCPclientService","Service created");
+		mServiceLooper = thread.getLooper();
+	    mServiceHandler = new ServiceHandler(mServiceLooper);
+	    
+
+	}
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO: Return the communication channel to the service.
 		throw new UnsupportedOperationException("Not yet implemented");
 	}
+	
+	 @Override
+	  public int onStartCommand(Intent intent, int flags, int startId) {
+	    Log.w("TCPclientService","Service started");
+	    Message msg = mServiceHandler.obtainMessage();
+	    msg.arg1 = startId;
+	    msg.setData(intent.getExtras());
+	    mServiceHandler.sendMessage(msg);
+	    
+	    
+	    
+	    return Service.START_NOT_STICKY;
+	  }
+	 
+	 public void onDestroy(){
+		 Log.w("TCPclientService","Service destroyed");
+	 }
+
+	 
 }
